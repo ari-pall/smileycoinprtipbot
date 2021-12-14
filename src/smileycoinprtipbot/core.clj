9076ab6864a91a02e22fe48bf132f1022227c50b
thisperson <thisperson@something.com>
2021-12-14 16:08:08 +0000

    ...

--- src/smileycoinprtipbot/core.clj
+++ src/smileycoinprtipbot/core.clj
@@ -45,27 +45,34 @@
                                                     :is "pr"
                                                     :merged (str ">" (get-timestamp-merges))}
                                                 {:oauth-token token})
-                              :items))]
+                              :items))
+        ;; _ (spit "merges args" (str [user token pr-reward-amount repo repo-name repo-owner]))
+        ;; _ (spit "new-merges" (str new-merges))
+        ]
     (doseq [pr new-merges]
       (let [pr-er (get-in pr [:user :login])
             prn (pr :number)]
+        (update-timestamp-merges (mergetime pr))
         (ti/create-comment repo-owner repo-name prn
                            (str "@" pr-er " Your pull request has been accepted. You can claim a Smileycoin reward by making a comment like this: `@" user " <your Smileycoin address>`")
-                           {:oauth-token token})
-        (update-timestamp-merges (mergetime pr))))))
+                           {:oauth-token token})))))
 
 (defn getbalance [] (read-string ((sh/sh "smileycoin-cli" "getbalance") :out)))
 
 (def txfee 0.921)
 
 (defn respond-to-mentions [{:keys [user token pr-reward-amount repo repo-name repo-owner]}]
-  (let [new-mentions (sort-by updatetimenumber
-                              ((ts/search-issues "" {:repo repo
-                                                     :is "merged"
-                                                     :updated (str ">" (get-timestamp-mentions))
-                                                     :mentions user}
-                                                {:oauth-token token})
-                               :items))]
+  (let [search-result (ts/search-issues "" {:repo repo
+                                            :is "merged"
+                                            :updated (str ">" (get-timestamp-mentions))
+                                            :mentions user}
+                                       {:oauth-token token})
+        new-mentions (sort-by updatetimenumber
+                              (search-result :items))
+        ;; _ (spit "mentions search result" (str search-result))
+        ;; _ (spit "mentions args" (str [user token pr-reward-amount repo repo-name repo-owner]))
+        ;; _ (spit "new-mentions" (str new-mentions))
+        ]
     (loop [remaining-mentions new-mentions]
       (if (and (not-empty remaining-mentions)
                (> (getbalance) (+ txfee pr-reward-amount)))
@@ -79,51 +86,35 @@
               last-mentioning-comment (find-if #(and (= pr-er (get-in % [:user :login]))
                                                      (clojure.string/includes? (% :body) (str "@" user)))
                                                (reverse comments))]
-          (do (update-timestamp-mentions (updatetime pr))
-              (if (and last-mentioning-comment (not bot-payment-comment))
-                (let [smlyaddress (str (first (last (re-seq #"([a-z]|[A-Z]|[0-9])+" (last-mentioning-comment :body)))))]
-                  (sh/sh "smileycoin-cli" "sendtoaddress" smlyaddress (str pr-reward-amount))
-                  (ti/create-comment repo-owner repo-name prn
-                                     (str "@" pr-er " " pr-reward-amount " Smileycoins have been sent to " smlyaddress)
-                                     {:oauth-token token})))
-              (recur (rest remaining-mentions))))))))
+          (update-timestamp-mentions (updatetime pr))
+          (if (and last-mentioning-comment (not bot-payment-comment))
+            (let [smlyaddress (str (first (last (re-seq #"([a-z]|[A-Z]|[0-9])+"
+                                                        (last-mentioning-comment :body)))))]
+              (sh/sh "smileycoin-cli" "sendtoaddress" smlyaddress (str pr-reward-amount))
+              (ti/create-comment repo-owner repo-name prn
+                                 (str "@" pr-er " " pr-reward-amount " Smileycoins have been sent to " smlyaddress)
+                                 {:oauth-token token})))
+          (recur (rest remaining-mentions)))))))
+
+(def second 1000)
+(def minute (* 60 second))
+(def hour (* 60 minute))
 
 (defn -main []
   (let [config (read-string ((sh/sh "cat" "config.edn") :out))
+        poll-time-minutes (config :poll-time-minutes)
         strings (clojure.string/split (config :repo) #"/")
         len (count strings)
         token (config :token)
-        config (assoc config :repo-owner (strings (- len 2))
-                             :repo-name (strings (- len 1))
+        repo-owner (strings (- len 2))
+        repo-name (strings (- len 1))
+        config (assoc config :repo-owner repo-owner
+                             :repo-name repo-name
+                             :repo (str repo-owner "/" repo-name)
                              :user ((tu/me {:oauth-token token}) :login))]
-    (respond-to-mentions config)
-    (respond-to-merges config)))
-
-
-
-
-;; (sh/sh "smileycoin-cli" "listtransactions")
-
-;; (def config (read-string ((sh/sh "cat" "config.edn") :out)))
-;; (def user (config :user))
-;; (def token (config :token))
-;; (let [strings (clojure.string/split (config :repo) #"/")
-;;       len (count strings)]
-;;   (def repo-owner (strings (- len 2)))
-;;   (def repo-name (strings (- len 1)))
-;;   )
-;; (def pr-reward-amount (config :pr-reward-amount))
-
-;; (map #(% :body) (ti/issue-comments repo-owner repo-name 4 {:oauth-token token}))
-
-;; ((sh/sh "smileycoin-cli" "help") :out)
-
-;; sendtoaddress newaddress pr-reward-amount
-
-;; (first (last (re-seq #"([a-z]|[A-Z]|[0-9])+" "@smlybot\n<sadfsas342f3>")))
-;; (str (first (last (re-seq #"([a-z]|[A-Z]|[0-9])+" "<<<<<<<<<"))))
-;; (re-find #"Smileycoins have been sent to" "Smileycoins have been sent    ")
-
-;; (range 10000)
-
-;; The options map only ever contains authentication info and/or optional input.
+    (loop []
+      (respond-to-mentions config)
+      (respond-to-merges config)
+      (spit "log" (str config))
+      (Thread/sleep (* minute poll-time-minutes))
+      (recur))))
